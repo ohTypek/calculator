c14c1953f30c87af7ad29ae85433b9a40a21c1f0
ohTypek <toptypek@wp.pl>
2022-08-17 09:43:54 +0200

    added possibility to click numbers and signs

---
+++ main.js
@@ -0,0 +1,15 @@
+let result;
+
+function addNumber(num) {
+    var equation = document.querySelector('#equation');
+    equation.innerHTML += num;
+    result += num;
+}
+function addSign(sign) {
+    var equation = document.querySelector('#equation');
+
+    equation.innerHTML += sign;
+}
+function result() {
+
+}
