2c8943ce477e35a42c71ba19c30fc73589b05e11
ohTypek <toptymek@gmail.com>
2022-08-18 09:14:15 +0200

    made script for calculator || can count simple equation

--- main.js
+++ main.js
@@ -1,15 +1,67 @@
-let result;
+let result = 0;
+let sResult = 0;
+let WSign;
+var isFh = false;
+var needNum = false;
+var resultReady = false;
+
 
 function addNumber(num) {
     var equation = document.querySelector('#equation');
-    equation.innerHTML += num;
-    result += num;
+    var FHResult = document.querySelector('#result-number');
+    let text = num.toString();
+
+    if (resultReady == true) {
+        equation.innerHTML = text;
+        FHResult.innerHTML = " "
+        resultReady = false;
+    } else {
+        equation.innerHTML += text;
+    }
+
+    if (isFh == false) {
+        result += text;
+        console.log(result);
+    } else {
+        if (needNum == false) {
+            equation.innerHTML = " ";
+            needNum = true;
+            equation.innerHTML += text;
+        }
+        sResult += text;
+    }
 }
 function addSign(sign) {
     var equation = document.querySelector('#equation');
+    var FHResult = document.querySelector('#result-number');
 
-    equation.innerHTML += sign;
+    if (isFh == false) {
+        equation.innerHTML = equation.innerHTML + " " + sign;
+        FHResult.innerHTML = equation.innerHTML;
+        WSign = sign;
+        isFh = true;
+    }
+    
 }
-function result() {
+function resultdisplay() {
+    console.log(Math.floor(result) + WSign + Math.floor(sResult));
 
+    var FHResult = document.querySelector('#result-number');
+    var equation = document.querySelector('#equation');
+
+    FHResult.innerHTML += equation.innerHTML;
+    if (WSign == '-') {
+        equation.innerHTML = Math.floor(result) - Math.floor(sResult)
+        console.log('substraction');
+    }
+    if (WSign == '+') {
+        equation.innerHTML = Math.floor(result) + Math.floor(sResult)
+        console.log('addition')
+    }
+    if (WSign == 'x') {
+        equation.innerHTML = Math.floor(result) * Math.floor(sResult)
+        console.log('multiplication')
+    }
+    resultReady = true;
+    isFh = false;
 }
